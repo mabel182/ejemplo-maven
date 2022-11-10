57a080211ef5ffe8a40972fde93fe1c627d4e843
Mabel <mabel.contreras182@gmail.com>
2022-11-09 21:14:04 -0300

    mejora bug null

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -19,21 +19,19 @@ import com.google.gson.Gson;
 @RestController
 @RequestMapping(path = "/rest/mscovid")
 public class RestData {
-	
+
 	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
-	
 	@GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Pais getData(@RequestParam(name = "msg") String message){
-		
+	public @ResponseBody Pais getData(@RequestParam(name = "msg") String message) {
+
 		LOGGER.log(Level.INFO, "Proceso exitoso de prueba");
-		
+
 		Pais response = new Pais();
 		response.setMensaje("Mensaje Recibido: " + message);
 		return response;
 	}
-	
-	
+
 	@GetMapping(path = "/estadoPais", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Pais getTotalPais(@RequestParam(name = "pais") String message){
 		RestTemplate restTemplate = new RestTemplate();
@@ -46,7 +44,8 @@ public class RestData {
 		int death = 0;
 		int recovered = 0;
 		Gson gson = new Gson();
-        Pais[] estados = gson.fromJson(call.getBody().toLowerCase(), Pais[].class);
+        Pais[] estados = call.getBody() !=null ? gson.fromJson(call.getBody().toLowerCase(), Pais[].class) : {};
+		
 
         for(Pais estado : estados) {
         	response.setDate(estado.getDate());
@@ -64,7 +63,6 @@ public class RestData {
 
 		return response;		
 	}
-	
 
 	@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Mundial getTotalMundial(){
@@ -75,7 +73,7 @@ public class RestData {
 	    ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/world/total" ,String.class);
 	    Mundial response = new Mundial();
 		Gson gson = new Gson();
-        Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
+        Mundial estado = call.getBody() != null ? gson.fromJson(call.getBody().toLowerCase(), Mundial.class) : {};
         response.setTotalConfirmed(estado.getTotalConfirmed());
         response.setTotalDeaths(estado.getTotalDeaths());
         response.setTotalRecovered(estado.getTotalRecovered());
